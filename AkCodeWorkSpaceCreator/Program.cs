fb3e072c7a791c1e9025a638856c5d8127e04ff5
Y.A <akf.type.r@gmail.com>
2021-02-13 18:49:51 +0900

    初期バージョン

--- AkCodeWorkSpaceCreator/Program.cs
+++ AkCodeWorkSpaceCreator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace AkCodeWorkSpaceCreator
 {
@@ -6,7 +8,36 @@ namespace AkCodeWorkSpaceCreator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var prg = new Program();
+            prg.Execute(args);
+        }
+
+        private void Execute(string[] args)
+        {
+            var currentPath = Environment.CurrentDirectory;
+            var dInfo = new DirectoryInfo(currentPath);
+
+            //VSCodeワークスペースファイルデータ
+            var data = new AkWorkSpaceData();
+            var folder = new AkFolderData();
+            data.Folders.Add(folder);
+
+            folder.Name = dInfo.Name;
+            folder.Path = ".";
+
+            //VSCodeワークスペースファイルJSONデータ
+            var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+            //VSCodeワークスペースファイル出力
+            var filePath = Path.Combine(currentPath, dInfo.Name + ".code-workspace");
+            File.WriteAllText(filePath, json);
+
+            //.vscodeフォルダ
+            var vscodeFolderPath = Path.Combine(currentPath, ".vscode");
+            if (!Directory.Exists(vscodeFolderPath))
+            {
+                Directory.CreateDirectory(vscodeFolderPath);
+            }
         }
     }
 }
